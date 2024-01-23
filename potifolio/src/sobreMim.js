804a6bc02ea68efef8a7765a28b8c6dc2832fccc
victor <133174073+victorlak@users.noreply.github.com>
2024-01-23 16:11:29 -0300

    acerto

--- potifolio/src/sobreMim.js
+++ potifolio/src/sobreMim.js
@@ -15,7 +15,7 @@ function SobreMim(){
                     </Row>
                     <Row>
                         <h6 className='d-flex justify-content-center' style={{ opacity: 0.8 }}>
-                            Ingressei na área no início de 2020, mais especificamente
+                            Ingressei na área no início de 2022, mais especificamente
                             em Ciência da Computação, no campus IFRP. Desde então venho
                             focando principalmente no aprendizado de desenvolvimento web.
                         </h6>
