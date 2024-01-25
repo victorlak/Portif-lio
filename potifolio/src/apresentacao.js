a63150b364cf507f862d6bdd848af443b7b9524b
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2024-01-24 21:43:44 -0300

    Commit
    adasds

--- potifolio/src/apresentacao.js
+++ potifolio/src/apresentacao.js
@@ -12,33 +12,30 @@ function Apresentacao(){
  
 
     return(
-        <Container className='apresentacao'>
+        <Container className='apresentacao '>
             <Row>
-                <Col className='infoBasicas font-weight-light' md='6'>
-                    <h2 className="display-4" >Olá, sou Victor Souza</h2>
-                    <h3 className="display-6 " style={{ opacity: 0.5 }}>
-                        Estudante de Ciência da Computação
-                        pelo instituto federal de Rio Pomba 
-                    </h3>
-                    <h5>
-                        Minhas habilidades:
-                    </h5>
-                    <Row className='mt-4'>
-                        <Col>
-                            <img src={cppImagem}/>
-                        </Col>
-                        <Col>
-                            <img src={cssImage}/>
-                        </Col>
-                        <Col>
-                            <img src={jsImage}/>
-                        </Col>
-                        <Col>
-                            <img src={reactImagem}/>
-                        </Col>
-                    </Row>
+                <Col className='infoBasicas mt-4 ms-5 text-center font-weight-light' >
+                    <Container className='ms-5'>
+                        <h2 className="display-4" >Olá, sou Victor Souza</h2>
+                        <h3 className="display-6 " style={{ opacity: 0.5 }}>
+                            Estudante de Ciência da Computação
+                            pelo instituto federal de Rio Pomba 
+                        </h3>
+                        <h5>
+                            Minhas habilidades:
+                        </h5>
+                        
+                           <Container className='images'>
+                                <img src={cppImagem}/>
+                                <img src={cssImage}/>
+                                <img src={jsImage}/>  
+                                <img src={reactImagem}/>
+                           </Container>
+                            
+                        
+                    </Container>
                 </Col>
-                <Col className='foto d-flex justify-content-end' md='6'>
+                <Col className='foto d-flex justify-content-center ' md='6'>
                     <img className='perfil' src={MinhaFT}/>
                 </Col>
             </Row>
